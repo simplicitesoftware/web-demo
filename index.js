15a84d37bd33981d6233fda1f623da8272ccf177
David AZOULAY <dazoulay@simplicite.fr>
2018-06-01 14:03:23 +0200

    Fix

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.login().then(function(params) {
 	var l = '<table class="products"><tbody>';
 	for (var i = 0; i < list.length; i++) {
 		var item = list[i];
-		l += '<tr><td><img src="data:image/png;base64,' + item.demoPrdPicture.thumbnail + '"/><td>';
+		l += '<tr><td><img src="data:image/png;base64,' + item.demoPrdPicture.thumbnail + '"/></td>';
 		l += '<td><div class="name">' + item.demoPrdName + '</div><div class="reference">' + item.demoPrdReference + '</div></td></tr>';
 	}
 	l += '</tbody></table>';
