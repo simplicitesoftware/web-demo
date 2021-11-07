fedd9bf8a0866163480776df717e01f76044c524
David AZOULAY <dazoulay@simplicite.fr>
2021-11-08 00:04:34 +0100

    Less loader

---
+++ webpack.config.development.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/main.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'main-bundle.js'
+  },
+  devServer: {
+    port: 3000
+  },
+  module: {
+    rules: [
+      {
+        test: /\.less$/i,
+        use: [
+          { loader: 'style-loader' },
+          { loader: 'css-loader' },
+          { loader: 'less-loader' }
+        ]
+      }
+    ]
+  }
+};
