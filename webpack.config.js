e31d23a180ec8ebbbedbebe174642663dcb76015
David AZOULAY <dazoulay@simplicite.fr>
2021-11-07 23:09:33 +0100

    Reorg

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,13 @@
+const path = require('path');
+
 module.exports = {
-  mode: 'development'
+  mode: 'development',
+  entry: './src/main.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'main.js'
+  },
+  devServer: {
+    port: 3000
+  }
 };
