06285b2da6caa9273bed742d4cb0ad3bffeeeffd
David AZOULAY <dazoulay@simplicite.fr>
2024-01-03 15:06:40 +0100

    Stylelint rules

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -2,7 +2,9 @@ module.exports = {
 	'extends': 'stylelint-config-standard',
 	'customSyntax': 'postcss-less',
 	'rules': {
+		'rule-empty-line-before': 'always',
 		'declaration-empty-line-before': 'never',
+		'declaration-block-no-redundant-longhand-properties': null,
 		'color-hex-length': 'long'
 	}
 };
