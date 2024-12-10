185df65b1adf299ada44ac3c053cc1a5f56a6f38
David AZOULAY <dazoulay@simplicite.fr>
2024-12-10 16:53:15 +0100

    ESLint config

---
+++ eslint.config.mjs
@@ -0,0 +1,10 @@
+export default [{
+    rules: {
+        "indent": ["error", "tab"],
+        "quotes": ["error", "single"],
+        "semi": ["error", "always"],
+        "no-multiple-empty-lines": ["error", { max: 1 }],
+        "no-multi-spaces": "error",
+        "no-trailing-spaces": [ "error", { skipBlankLines: false } ]
+    }
+}];
